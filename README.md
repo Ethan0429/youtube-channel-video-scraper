4811f63da1de2f3fc404b0b38e3e8f7451c3f47c
Ethan Rickert <ethanrickert0429@gmail.com>
2022-12-19 06:03:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# YouTube Channel-Video Scraper
+
+## About
+
+This is a simple script that scrapes a YouTube channel for all the videos on the channel. It uses Selenium to scroll through the channel page, and then parses the HTML to get the video URLs and titles.
+
+## Requirements
+
+* Python 3.6+
+* Selenium
+
+### Pip
+
+All requirements are listed in `requirements.txt`, and can be installed with `pip install -r requirements.txt`
+
+### Poetry
+
+Alternatively, you can use [Poetry](https://python-poetry.org/) to install the dependencies. To do so, run `poetry install` in the project directory, as the dependencies are listed in `pyproject.toml`.
+
+## Usage
+
+There are two ways to use this script. The first is to run it directly from the command line, and the second is to import it into your own project.
+
+### CLI/Args
+
+```
+usage: YouTubeScraper.py [-h] [-o OUTPUT_FILE] [-delay DELAY] [-strip] [-head] channel_id driver
+```
+
+#### Help
+```
+usage: YouTubeScraper.py [-h] [-o OUTPUT_FILE] [-delay DELAY] [-strip] [-head] channel_id driver
+
+positional arguments:
+  channel_id            YouTube channel ID
+  driver                Path to chromedriver executable
+
+options:
+  -h, --help            show this help message and exit
+  -o OUTPUT_FILE, --output OUTPUT_FILE
+                        Output file name/path
+  -delay DELAY, --delay DELAY
+                        Delay between page loads
+  -strip, --strip       Strip special characters
+  -head, --head         Run driver without headless mode
+```
+
+In total there are 6 arguments, only 2 of which are required.
+
+#### Required
+
+* `channel_id` - The YouTube channel ID. This can be found in the URL of the channel, or by using the [YouTube Data API](https://developers.google.com/youtube/v3/docs/channels/list).
+* `driver` - The path to the chromedriver executable. This can be downloaded from [here](https://chromedriver.chromium.org/downloads).
+
+#### Optional
+
+* `-o OUTPUT_FILE, --output OUTPUT_FILE` - The output file name/path. If not specified, then the objects will be stored in `YouTubeScraper.videos`.
+* `-delay DELAY, --delay DELAY` - The delay between page scrolls. Pretty minimal, but can be useful if you're browser is loading slow. Default is 1.1 seconds which seems to work fine for me.
+* `-strip, --strip` - Strip special characters from the video titles. This is useful if you're planning on using the output in a CSV file. Doesn't cover all bases, but it does the job for me. I don't recommend you use it since it's not perfect, AND it's mostly for my own purposes.
+* `-head, --head` - Run the driver without headless mode. Useful for debugging.
+
+### API
+
+The script can also be imported into your own project. The `YouTubeScraper` class is the main class. It has one exposed method, `scrape`. It takes all the arguments listed in [CLI/Args](#Help), and returns a list of `Video` objects.
+
+```python
+from YouTubeScraper import YouTubeScraper
+
+scraper = YouTubeScraper(channel_id, driver) # Create a new scraper object
+scraper.scrape() # Scrape the channel
+videos = scraper.videos # Get the scraped videos
+```
+
+The `YouTubeScraper` object contains a `video` attribute, which is a list of tuples containing the video url (`video[0]`) and the video title (`video[1]`). They're both strings.
---
+++ YouTubeScraper.py
@@ -0,0 +1,149 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import argparse
+
+
+class YouTubeScraper:
+    def __init__(self, channel_id, driver_path, output_file, headless, delay, strip_special_characters):
+        self.channel_id = channel_id
+        self.output_file = output_file
+        self.delay = delay
+        self.strip_special_characters = strip_special_characters
+        self.videos = []
+        self.driver = None
+        self.driver_path = driver_path
+        self.headless = headless
+
+    def __init_driver(self):
+        if self.headless:
+            options = webdriver.ChromeOptions()
+            options.add_argument('headless')
+            self.driver = webdriver.Chrome(
+                executable_path=self.driver_path, options=options)
+
+        else:
+            self.driver = webdriver.Chrome(
+                executable_path=self.driver_path)
+
+        self.driver.get(
+            f'https://www.youtube.com/channel/{self.channel_id}/videos')
+
+        # wait for page to load
+        wait = WebDriverWait(self.driver, 10)
+
+    def __scroll(self):
+        last_height = 0
+        current_height = self.driver.execute_script(
+            "return document.documentElement.scrollHeight")
+
+        # continuously scroll down page until all videos are loaded
+
+        while last_height != current_height:
+            last_height = current_height
+
+            # wait for body tag to load
+            wait = WebDriverWait(self.driver, 10)
+            wait.until(EC.presence_of_element_located(
+                (By.TAG_NAME, 'body')))
+
+            # scroll down page
+            self.driver.find_element(
+                By.TAG_NAME, 'body').send_keys(Keys.END)
+
+            time.sleep(self.delay)
+
+            # update page height
+            current_height = self.driver.execute_script(
+                "return document.documentElement.scrollHeight")
+
+    def __get_videos(self):
+        self.videos = [(str(video.get_attribute("href")), str(video.get_attribute("title")))
+                       for video in self.driver.find_elements(By.ID, 'video-title-link')]
+
+        # strip special characters from titles
+        if self.strip_special_characters:
+            self.videos = [(video[0], self.__strip_special_characters(
+                video[1])) for video in self.videos]
+
+    def __strip_special_characters(self, title):
+        new_title = title
+        new_title = new_title.replace('/', '')
+        new_title = new_title.replace(':', '')
+        new_title = new_title.replace('(', '')
+        new_title = new_title.replace(')', '')
+        new_title = new_title.replace('?', '')
+        new_title = new_title.replace('!', '')
+        new_title = new_title.replace('*', '')
+        new_title = new_title.replace('"', '')
+        new_title = new_title.replace('<', '')
+        new_title = new_title.replace('>', '')
+        new_title = new_title.replace('|', '')
+        new_title = new_title.replace('\'', '')
+        new_title = new_title.replace(',', '')
+        new_title = new_title.replace('&amp', 'and')
+        new_title = new_title.replace('&#39;', '')
+        new_title = new_title.replace('&quot;', '')
+        new_title = new_title.replace('\u2013', '')
+
+        new_title = new_title.strip()
+
+        return new_title
+
+    def __write_to_file(self):
+        video_count = 0
+        with open(self.output_file, 'w') as f:
+            f.write('"URL","Title"\n')
+            for video in self.videos:
+                try:
+                    f.write(
+                        f'"{video[0]}","{video[1]}"\n')
+                    video_count += 1
+                except:
+                    print(f'Error writing {video[1]}, {video[0]} to file.')
+
+        print(f'Wrote {video_count} videos to file.')
+
+    def scrape(self):
+        self.__init_driver()
+        self.__scroll()
+        self.__get_videos()
+
+        if self.output_file:
+            self.__write_to_file()
+
+        self.driver.close()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('channel_id', help='YouTube channel ID',
+                        type=str)
+    parser.add_argument('driver', help='Path to chromedriver executable',
+                        type=str)
+    parser.add_argument('-o', '--output', help='Output file name/path',
+                        type=str, required=False, metavar='OUTPUT_FILE')
+    parser.add_argument('-delay', '--delay',
+                        help='Delay between page loads', type=float, required=False, default=1.1)
+    parser.add_argument('-strip', '--strip',
+                        help='Strip special characters', required=False, default=False, action='store_true')
+    parser.add_argument('-head', '--head',
+                        help='Run driver without headless mode', required=False, default=True, action='store_false')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    scraper = YouTubeScraper(args.channel_id, args.driver, output_file=args.output,
+                             delay=args.delay, strip_special_characters=args.strip, headless=args.head)
+
+    scraper.scrape()
+
+
+if __name__ == '__main__':
+    exit(main())
